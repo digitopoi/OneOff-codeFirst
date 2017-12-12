2badd7cc7a667b308c3fc363dff417f78ce874a1
digitopoi <hubo85@gmail.com>
2017-12-12 14:51:09 -0600

    Add Artist controller

--- OneOff.Web.MVC/Controllers/ArtistController.cs
+++ OneOff.Web.MVC/Controllers/ArtistController.cs
@@ -9,30 +9,30 @@ using System.Web;
 using System.Web.Mvc;
 using OneOff.Data.Entities;
 using OneOff.Web.MVC.Models;
+using OneOff.Services;
+using Microsoft.AspNet.Identity;
+using OneOff.Models;
 
 namespace OneOff.Web.MVC.Controllers
 {
     public class ArtistController : Controller
     {
         // GET: Artist
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
-            return View(await db.Gigs.ToListAsync());
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new GigService(userId);
+            var model = service.GetGigsByUser();
+            return View(model);
         }
 
         // GET: Artist/Details/5
-        public async Task<ActionResult> Details(int? id)
+        public async Task<ActionResult> Details(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GigEntity gig = await db.Gigs.FindAsync(id);
-            if (gig == null)
-            {
-                return HttpNotFound();
-            }
-            return View(gig);
+            var service = CreateGigService();
+            var model = await service.GetGigByIdAsync(id);
+
+            return View(model);
         }
 
         // GET: Artist/Create
@@ -42,66 +42,62 @@ namespace OneOff.Web.MVC.Controllers
         }
 
         // POST: Artist/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "GigId,OwnerId,Date,PostalCode")] GigEntity gig)
+        public async Task<ActionResult> Create(GigViewModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                db.Gigs.Add(gig);
-                await db.SaveChangesAsync();
+                return View(model);
+            }
+
+            var service = CreateGigService();
+            
+            if(await service.CreateGigAsync(model, true))
+            {
+                TempData["SaveResult"] = "Your note was created.";
                 return RedirectToAction("Index");
             }
 
-            return View(gig);
+            ModelState.AddModelError("", "Note could not be created.");
+            return View(model);
         }
 
         // GET: Artist/Edit/5
-        public async Task<ActionResult> Edit(int? id)
+        public async Task<ActionResult> Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GigEntity gig = await db.Gigs.FindAsync(id);
-            if (gig == null)
-            {
-                return HttpNotFound();
-            }
-            return View(gig);
+            var service = CreateGigService();
+            var model = await service.GetGigByIdAsync(id);
+
+            return View(model);
         }
 
         // POST: Artist/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "GigId,OwnerId,Date,PostalCode")] GigEntity gig)
+        public async Task<ActionResult> Edit(int id, GigViewModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateGigService();
+
+            if (await service.UpdateGigAsync(id, model))
             {
-                db.Entry(gig).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                TempData["SaveResult"] = "Your gig was updated";
                 return RedirectToAction("Index");
             }
-            return View(gig);
+
+            ModelState.AddModelError("", "Your gig could not be updated.");
+            return View(model);
         }
 
         // GET: Artist/Delete/5
-        public async Task<ActionResult> Delete(int? id)
+        public async Task<ActionResult> Delete(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GigEntity gig = await db.Gigs.FindAsync(id);
-            if (gig == null)
-            {
-                return HttpNotFound();
-            }
-            return View(gig);
+            var service = CreateGigService();
+            var model = await service.GetGigByIdAsync(id);
+
+            return View(model);
         }
 
         // POST: Artist/Delete/5
@@ -109,19 +105,19 @@ namespace OneOff.Web.MVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            GigEntity gig = await db.Gigs.FindAsync(id);
-            db.Gigs.Remove(gig);
-            await db.SaveChangesAsync();
+            var service = CreateGigService();
+            await service.DeleteGigAsync(id);
+
+            TempData["SaveResult"] = "Your gig was cancelled";
+
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
+        private GigService CreateGigService()
         {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new GigService(userId);
+            return service;
         }
     }
 }
