b040d92dff64dc6315206902aa7f1d0b1002689e
digitopoi <hubo85@gmail.com>
2017-12-12 17:08:50 -0600

    Add artist and venue roles

--- OneOff.Web.MVC/Startup.cs
+++ OneOff.Web.MVC/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using OneOff.Web.MVC.Models;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(OneOff.Web.MVC.Startup))]
@@ -9,6 +12,31 @@ namespace OneOff.Web.MVC
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            var context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //  create artist role
+            if (!roleManager.RoleExists("Artist"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Artist";
+                roleManager.Create(role);
+            }
+
+            //  create venue role
+            if (!roleManager.RoleExists("Venue"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Venue";
+                roleManager.Create(role);
+            }
         }
     }
 }
