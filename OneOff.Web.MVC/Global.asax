a7698279774060f3bd9a87058954b03cc506bde9
digitopoi <hubo85@gmail.com>
2017-12-12 09:18:22 -0600

    Add project files.

---
+++ OneOff.Web.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneOff.Web.MVC.MvcApplication" Language="C#" %>
---
+++ OneOff.Web.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneOff.Web.MVC.Startup))]
+namespace OneOff.Web.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
